c4559af69b48e72cb4274eb104420fafafe3b7d3
Iwaide <iwaide@hoobby.net>
2024-06-19 17:58:16 +0900

    シミュレーションする方の解き方でも解いてみた

--- agc016/a/main.cpp
+++ agc016/a/main.cpp
@@ -3,20 +3,43 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
 
-int func(string s, char c) {
-  vector<int> tmp;
-  int ans = 0;
-  rep(i, s.size()) {
-    if (s.at(i) == c) continue;
+// int func(string s, char c) {
+//   vector<int> tmp;
+//   int ans = 0;
+//   rep(i, s.size()) {
+//     if (s.at(i) == c) continue;
 
-    int cnt = s.size() - i;
-    rep2(j, i + 1, s.size()) {
-      if (s.at(j) == c) {
-        cnt = min(cnt, j - i);
-        break;
+//     int cnt = s.size() - i;
+//     rep2(j, i + 1, s.size()) {
+//       if (s.at(j) == c) {
+//         cnt = min(cnt, j - i);
+//         break;
+//       }
+//     }
+//     ans = max(cnt, ans);
+//   }
+//   return ans;
+// }
+
+
+int func(string s, char c) {
+  set<char> sets;
+  int ans = 0;
+  while(s.size() > 1) {
+    for(char c: s) sets.insert(c);
+    if (sets.size() == 1) break;
+
+    sets = {};
+    rep(i, s.size() - 1) {
+      if (s.at(i) == c || s.at(i + 1) == c) {
+        s.at(i) = c;
+        sets.insert(c);
+      } else {
+        sets.insert(s.at(i));
       }
     }
-    ans = max(cnt, ans);
+    s.erase(s.size() - 1, 1);
+    ans++;
   }
   return ans;
 }
