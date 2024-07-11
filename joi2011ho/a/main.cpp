d07b7feede731cc93a3b2911fba6b6b858d4eca8
Iwaide <iwaide@hoobby.net>
2024-07-11 11:40:02 +0900

    7/11: 2次元累積和をちょっと理解した。面積を求めるときは大きいやつから小さいやつを引いて、重複して引いたところを足す

--- joi2011ho/a/main.cpp
+++ joi2011ho/a/main.cpp
@@ -3,31 +3,31 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
 
-int main() {
+int main()
+{
   int M, N, K;
   cin >> M >> N >> K;
-  vector<vector<int>> J(M, vector<int>(N + 1, 0));
-  vector<vector<int>> O(M, vector<int>(N + 1, 0));
-  vector<vector<int>> I(M, vector<int>(N + 1, 0));
-  rep(i, M) {
-    rep(j, N) {
-      char c; cin >> c;
-      J.at(i).at(j + 1) = J.at(i).at(j) + (c == 'J' ? 1 : 0);
-      I.at(i).at(j + 1) = I.at(i).at(j) + (c == 'I' ? 1 : 0);
-      O.at(i).at(j + 1) = O.at(i).at(j) + (c == 'O' ? 1 : 0);
+  vector<vector<int>> J(M + 1, vector<int>(N + 1, 0));
+  vector<vector<int>> O(M + 1, vector<int>(N + 1, 0));
+  vector<vector<int>> I(M + 1, vector<int>(N + 1, 0));
+  vector<vector<char>> F(M, vector<char>(N));
+
+  rep(y, M) rep(x, N) cin >> F[y][x];
+  rep(y, M) {
+    rep(x, N) {
+      J[y + 1][x + 1] = J[y][x + 1] + J[y + 1][x] - J[y][x] + (F[y][x] == 'J' ? 1 : 0);
+      I[y + 1][x + 1] = I[y][x + 1] + I[y + 1][x] - I[y][x] + (F[y][x] == 'I' ? 1 : 0);
+      O[y + 1][x + 1] = O[y][x + 1] + O[y + 1][x] - O[y][x] + (F[y][x] == 'O' ? 1 : 0);
     }
   }
   rep(i, K) {
-    int a, b, c, d;
-    cin >> a >> b >> c >> d;
-    a--; c--;
-    int Ic = 0, Oc = 0, Jc = 0; 
-    for(int y = a; y <= c; y++) {
-      Ic += I.at(y).at(d) - I.at(y).at(b - 1);
-      Oc += O.at(y).at(d) - O.at(y).at(b - 1);
-      Jc += J.at(y).at(d) - J.at(y).at(b - 1);
-    }
+    int y1, x1, y2, x2;
+    cin >> y1 >> x1 >> y2 >> x2;
+    y1--; x1--; y2--; x2--;
+    int Jc = J[y2 + 1][x2 + 1] - J[y1][x2 + 1] - J[y2 + 1][x1] + J[y1][x1];
+    int Ic = I[y2 + 1][x2 + 1] - I[y1][x2 + 1] - I[y2 + 1][x1] + I[y1][x1];
+    int Oc = O[y2 + 1][x2 + 1] - O[y1][x2 + 1] - O[y2 + 1][x1] + O[y1][x1];
     cout << Jc << " " << Oc << " " << Ic << endl;
   }
+  return 0;
 }
-
