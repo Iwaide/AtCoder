6157bab7ef3f296bac2fdd4cab6c26958435138c
Iwaide <iwaide@hoobby.net>
2024-08-07 19:53:40 +0900

    8/7: DP解き直しとabc365D。なんとか一個できた気がする

--- abc365/d/main.cpp
+++ abc365/d/main.cpp
@@ -1,8 +1,104 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define rep(i, n) for (int i = 0; i < (int)(n); i++)
+// #define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+
+// int N;
+// string S;
+
+// char win(char c) {
+//     if (c == 'R') return 'P';
+//     else if (c == 'P') return 'S';
+//     else return 'R';
+// }
+
+// // 幅優先探索
+// int rec(int index, char before_hand) {
+//   char win_hand = win(S[index]);
+//   if (index == S.size() - 1) {
+//     if (win_hand == before_hand) {
+//       return 0;
+//     } else {
+//       return 1;
+//     }
+//   }
+
+//   int ans1 = -1;
+//   if (win_hand != before_hand) {
+//     ans1 = 1 + rec(index + 1, win_hand);
+//   }
+//   int ans2 = -1;
+//   if (S[index] != before_hand) {
+//     ans2 = 0 + rec(index + 1, S[index]);
+//   }
+//   return max(ans1, ans2);
+// }
+
+// vector<vector<char>> dp;
+// int main() {
+//   cin >> N >> S;
+
+//   int ans1 = rec(0, 'N');
+//   cout << ans1 << endl;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define rep(i, n) for (int i = 0; i < (int)(n); i++)
+// #define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+
+// int N;
+// string S;
+// vector<vector<int>> dp;
+
+// char win(char c) {
+//     if (c == 'R') return 'P';
+//     else if (c == 'P') return 'S';
+//     else return 'R';
+// }
+
+// // メモ化再帰
+// int rec(int index, char before_hand) {
+//     if (dp[index][before_hand - 'A'] != -1) return dp[index][before_hand - 'A'];
+
+//     char win_hand = win(S[index]);
+//     if (index == S.size() - 1) {
+//         if (win_hand == before_hand) {
+//             return dp[index][before_hand - 'A'] = 0;
+//         } else {
+//             return dp[index][before_hand - 'A'] = 1;
+//         }
+//     }
+
+//     int ans1 = -1;
+//     if (win_hand != before_hand) {
+//         ans1 = 1 + rec(index + 1, win_hand);
+//     }
+//     int ans2 = -1;
+//     if (S[index] != before_hand) {
+//         ans2 = 0 + rec(index + 1, S[index]);
+//     }
+//     return dp[index][before_hand - 'A'] = max(ans1, ans2);
+// }
+
+// int main() {
+//     cin >> N >> S;
+
+//     // dpを初期化する。各要素は-1で初期化される
+//     dp.assign(N, vector<int>(26, -1)); // 26はアルファベットの数
+
+//     int ans1 = rec(0, 'N'); // 'N'は初期状態で使われるダミー文字
+//     cout << ans1 << endl;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
 
 int N;
 string S;
+vector<map<char, int>> dp;
 
 char win(char c) {
     if (c == 'R') return 'P';
@@ -11,30 +107,40 @@ char win(char c) {
 }
 
 int main() {
-    cin >> N >> S;
-    
-    // dp[i][j]: i回目のジャンケンで手j（0: 'R', 1: 'P', 2: 'S'）を出した時の勝利回数の最大値
-    vector<vector<int>> dp(N + 1, vector<int>(3, 0));
+  cin >> N >> S;
+  dp.resize(N);
+  rep(i, N) {
+    dp.push_back(
+      map<char, int> {
+        { 'P', -1 },
+        { 'R', -1 },
+        { 'S', -1 },
+      }
+    );
+  }
+  char first_hand = S[0];
+  dp[0][first_hand] = 0;
+  dp[0][win(first_hand)] = 1;
+  vector<char> hands{ 'R', 'P', 'S' };
+  rep2(i, 1, N) {
+    char s = S[i];
 
-    for (int i = 0; i < N; ++i) {
-        char a_hand = S[i];
-        char next_winning_hand = win(a_hand);
-
-        int next_winning_hand_index = (next_winning_hand == 'R') ? 0 : (next_winning_hand == 'P') ? 1 : 2;
-        int a_hand_index = (a_hand == 'R') ? 0 : (a_hand == 'P') ? 1 : 2;
-
-        for (int j = 0; j < 3; ++j) {
-            if (j != next_winning_hand_index) {
-                dp[i + 1][next_winning_hand_index] = max(dp[i + 1][next_winning_hand_index], dp[i][j] + 1);
-            }
-            if (j != a_hand_index) {
-                dp[i + 1][a_hand_index] = max(dp[i + 1][a_hand_index], dp[i][j]);
-            }
-        }
+    rep(j, hands.size()) {
+      char hand = hands[j];
+      int tmp = INT_MIN;
+      // あいこのとき
+      if (hand != s) {
+        tmp = max(tmp, dp[i - 1][hand]);
+      }
+      dp[i][s] = max(dp[i][s], tmp);
+      // 勝ちのとき
+      char win_hand = win(s);
+      tmp = INT_MIN;
+      if (hand != win_hand) {
+        tmp = max(tmp, dp[i - 1][hand]) + 1;
+      }
+      dp[i][win_hand] = max(dp[i][win_hand], tmp);
     }
-
-    int maxVictory = max({dp[N][0], dp[N][1], dp[N][2]});
-    cout << maxVictory << endl;
-
-    return 0;
+  }
+  cout << max(dp[N - 1]['P'], max(dp[N - 1]['R'], dp[N - 1]['S'])) << endl;
 }
--- dp/a/main.cpp
+++ dp/a/main.cpp
@@ -7,83 +7,55 @@ using namespace std;
 // 足場1からカエルスタート。i+1 か i+2に飛ぶ
 // 飛んだ足場の高さの差がコストになる
 // 足場Nにたどり着くまでのコストの総和の最小値
-
 int N;
 vector<long long> h;
-long long INF = 1LL << 60;
 
-// // 幅優先で解いてみた。TLEする
-// int rec(int i) {
-//   if (i == 0) {
+// メモ化再帰で解く
+// vector<long long> dp;
+// long long rec(int index) {
+//   if (dp[index] != -1) {
+//     return dp[index];
+//   }
+//   if (index == 0) {
 //     return 0;
 //   }
-//   long long result = INF;
-//   // i - 1からきたとき
-//   if (i - 1 >= 0) {
-//     result = min(result, rec(i - 1) + abs(h[i] - h[i - 1]));
-//   }
-//   if (i - 2 >= 0) {
-//     result = min(result, rec(i - 2) + abs(h[i] - h[i - 2]));
+
+//   long long ans1 = LONG_LONG_MAX;
+//   if (index - 1 >= 0) {
+//     ans1 = rec(index - 1) + abs(h[index] - h[index - 1]);
 //   }
 
-//   return result;
+//   long long ans2 = LONG_LONG_MAX;
+//   if (index - 2 >= 0) {
+//     ans2 = rec(index - 2) + abs(h[index] - h[index - 2]);
+//   }
+//   return dp[index] = min(ans1, ans2);
 // }
 
 // int main() {
 //   cin >> N;
-//   h.resize(N, 0);
+//   h.resize(N);
+//   dp.resize(N, -1);
 //   rep(i, N) cin >> h[i];
-
 //   long long ans = rec(N - 1);
-
-//   cout << ans << endl;
-// }
-
-// メモ化する
-// vector<long long> memo;
-// int rec(int i) {
-//   if (memo[i] != -1) {
-//     return memo[i];
-//   }
-
-//   if (i == 0) {
-//     return 0;
-//   }
-
-//   long long result = INF;
-//   // i - 1からきたとき
-//   if (i - 1 >= 0) {
-//     result = min(result, rec(i - 1) + abs(h[i] - h[i - 1]));
-//   }
-//   if (i - 2 >= 0) {
-//     result = min(result, rec(i - 2) + abs(h[i] - h[i - 2]));
-//   }
-
-//   return memo[i] = result;
-// }
-
-// int main() {
-//   cin >> N;
-//   h.resize(N, 0);
-//   memo.resize(N, -1);
-//   rep(i, N) cin >> h[i];
-
-//   long long ans = rec(N - 1);
-
 //   cout << ans << endl;
 // }
 
 // DPで解く
+vector<long long> dp;
 int main() {
   cin >> N;
   h.resize(N);
   rep(i, N) cin >> h[i];
-  vector<long long> dp(N, LONG_LONG_MAX);
-  dp[0] = 0;
-  for(int i = 1; i < N; i++) {
-    dp[i] = min(dp[i], dp[i - 1] + abs(h[i] - h[i - 1]));
 
-    if (1 < i) {
+  dp.resize(N, LONG_LONG_MAX);
+  dp[0] = 0;
+
+  for(int i = 1; i < N; i++) {
+    if (i - 1 >= 0) {
+      dp[i] = dp[i - 1] + abs(h[i] -h[i - 1]);
+    }
+    if (i - 2 >= 0) {
       dp[i] = min(dp[i], dp[i - 2] + abs(h[i] - h[i - 2]));
     }
   }
