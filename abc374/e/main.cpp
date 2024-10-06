5b64f038e54bf7b404d3a852456c0eddd8f0bd75
Iwaide <iwaide@hoobby.net>
2024-10-06 23:14:09 +0900

    10/6: ABC374Eの解き直し

--- abc374/e/main.cpp
+++ abc374/e/main.cpp
@@ -1,10 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
-using ll = long long;
+template<class T> inline ll div_ceil(T a,T b) { return (a+b-1)/b; }
+
 #define all(a) (a).begin(), (a).end()
 template<typename T>
 void print_vector(vector<T> &vec) {
@@ -17,59 +20,56 @@ void print_vector(vector<T> &vec) {
   }
 }
 
+
 // ある製品には1...N個の工程が必要
 // 各工程iについてそれを処理する2種類の機械、Si, Tiが売られている
-// Ai個/日、Pi円
-// Bi個/日、Qi円
+// Si: Ai個/日、Pi円
+// Ti: Bi個/日、Qi円
 // 予算がX円のとき達成可能な製造能力の最大値
-// 普通に実装すると予算ぶんだけ探索する必要があって大変そう
-// dpやるんだろうけどキーがわからんな。
-// 個数によってdpやって全部足してX円超えなかったとき？
+// 機械Siのほうがコスパ悪いとき、機械Siの導入台数は必ずBi台未満になる
+// Bi台のSiによる製造能力(Ai * Bi)はAi台の機械Tiによる製造能力(Bi * Ai)で代替できるから
 int main() {
-  int N, X;
-  cin >> N >> X;
-  vector<vector<double>> M(N, vector<double>(4));
-  rep(i, N) {
-    double A, P, B, Q;
-    cin >> A >> P >> B >> Q;
-    M[i] = { A, P, B, Q };
+  int n, x;
+  cin >> n >> x;
+  vector<ll> A(n), P(n), B(n), Q(n);
+  rep(i, n) {
+    cin >> A[i] >> P[i] >> B[i] >> Q[i];
   }
-  // 個数でループ
-  rep2(i, 1, 10000001) {
-    int total = 0;
-    // 各工程
-    rep(j, N) {
-      auto m = M[j];
-      double A = m[0], P = m[1], B = m[2], Q = m[3];
-      int cost = INT_MAX;
-      // コスパ最大までとったケースと、1個減らして超えるまで足したケース
-      double costA = A / P, costB = B / Q;
-      // APの機械のほうが安いとき
-      if (costA < costB) {
-        int costA = 0, costAB = 0;
-        // 全部APでまかなったとき
-        int numA = (i + A - 1) / A;
-        costA = numA * P;
-        // numA-1個のAとBでまかなったとき
-        int numB = (i - (numA - 1) * A + B - 1) / B;
-        costAB = (numA - 1) * P + numB * Q;
-        cost = min(costA, costAB);
-      } else {
-        int costB = 0, costAB = 0;
-        // 全部Bでまかなったとき
-        int numB = (i + B - 1) / B;
-        costB = numB * Q;
-        // numA-1個のBとAでまかなったとき
-        numB--;
-        int numA = (i - numB * B + A - 1) / A;
-        costAB = numB * Q + numA * P;
-        cost= min(costB, costAB);
+  ll l = -1, r = x * 100 + 1;
+  while(r - l > 1) {
+    ll mid = (l + r) / 2;
+    bool can = true;
+    // かかる金額
+    ll sum = 0;
+
+    // 各工程について
+    rep(i, n) {
+      // aはTiのほうがコスパ悪いときにかかる最小金額
+      // bはSiのほうがコスパ悪いときにかかる最小金額
+      ll a = 1e9, b = 1e9;
+      // Tiのほうがコスパ悪いとき、TiはAi台未満になるはずなので全探索する
+      rep(j, A[i]) {
+        ll TiSum = j * Q[i];
+        ll SiSum = div_ceil(max(0LL, mid - B[i] * j), A[i]) * P[i];
+        chmin(a, SiSum + TiSum);
       }
-      total += cost;
+      // Siのほうがコスパ悪い時SiはBi台未満になるはず
+      rep(j, B[i]) {
+        ll SiSum = j * P[i];
+        ll TiSum = div_ceil(max(0LL, mid - A[i] * j), B[i]) * Q[i];
+        chmin(b, SiSum + TiSum);
+      }
+      sum += min(a, b);
+      if (sum > x) {
+        can = false;
+        break;
+      }
     }
-    if (total > X) {
-      cout << (i - 1) << endl;
-      break;
+    if (can) {
+      l = mid;
+    } else {
+      r = mid;
     }
   }
+  cout << l << endl;
 }
