1a022ba5dc9d5978d12c3c83ee0d87e9df3accf1
Iwaide <iwaide@hoobby.net>
2024-11-27 23:17:59 +0900

    11/27: ABC367D、なんとなくは分かるんだけど初見で解ける気がしないなぁ。深く考えるっていうのが難しい…

--- abc367/d/main.cpp
+++ abc367/d/main.cpp
@@ -8,19 +8,32 @@ using ll = long long;
 #define all(a) (a).begin(), (a).end()
 
 int main() {
-  ll N, M;
-  cin >> N >> M;
-  vector<ll> A(N);
-  rep(i, N) cin >> A[i];
-  vector<ll> csum(2 * N + 1, 0);
-  vector<set<int>> mod(M);
-  mod[0].insert(0);
-  
-  rep(i, N * 2) {
-    csum[i + 1] = csum[i] + A[i % N];
-    mod[csum[i+1] % M].insert(i+1);
+  int n,m;
+  cin >> n >> m;
+  vector<int> a(n);
+  rep(i, n) cin >> a[i];
+  vector<int> r={0};
+  // 累積和の余りを求める
+  rep(i, 2 * n) {
+    r.push_back((r.back() + a[i % n]) % m);
   }
-  // -------------
-  cout << "hoge" << endl;
-}
+  vector<int> b(m, 0);
+  // 0, N - 1までのあまりの頻度を求めておく
+  rep(i, n) b[r[i]]++;
 
+  ll res = 0;
+  // Nから 2 * N - 1までについて計算する
+  // s < tのとき Rs = Rt つまり RN + s = RN + t
+  // s > tのとき Rs = RN + t だから i = N,.. 2N - 1について
+  // Ri - N + 1 ,,, Ri - 1の中にRiと等しい要素がどれだけあるかカウントする
+  for(int i = n; i < 2 * n; i++) {
+    // B[Ri - N]から1引く
+    // Bはi　- N + 1からi-1までの頻度を管理する配列となる
+    b[r[i-n]]--;
+    // r[i]の頻度を足す
+    res += b[r[i]];
+    // B[R[i]]に1加算する Bはi - N + 1からiまでの頻度を管理する配列となる
+    b[r[i]]++;
+  }
+  cout << res << endl;
+}
