36f80849975414f27be5512b00cfa5e247fa943b
Iwaide <iwaide@hoobby.net>
2024-12-03 22:19:00 +0900

    12/3: agc032A、逆の操作をする発想

--- agc032/a/main.cpp
+++ agc032/a/main.cpp
@@ -13,50 +13,33 @@ void print_vector(vector<T> &vec) {
   }
 }
 
-int N;
-vector<int> a, b, ans;
-map<int, int> mapB;
-bool dfs(int i) {
-  if (i == N + 1) {
-    if (a == b) {
-      print_vector(ans);
-      return true;
-    } else  {
-      return false;
-    }
-  }
-  bool res = false;
-  // indexより小さい数を突っ込んでみる
-  for(int j = 1; j <= i; j++) {
-    if (mapB[j] > 0) {
-      mapB[j]--;
-      a.insert(a.begin() + j - 1, j);
-      ans.push_back(j);
-      res |= dfs(i + 1);
-      a.erase(a.begin() + j - 1);
-      ans.pop_back();
-      mapB[j]++;
-    }
-  }
-  return res;
-}
-
 // すぬけ君は空の数列aを持っている
 // aに対してN回の操作を行う
 // i回目の操作では1 <= j <= iを満たす整数jを選びaの先頭からj番目にjを挿入するとができる
 // 長さNの数列bが与えられる、N回の操作後にaがbと一致することがあるかどうか判定し
 // 可能ならばそれを達成する操作手順の一例を示せ
+// 操作を逆順に考える、という発送がなかった
 int main() {
-  cin >> N;
-  b.resize(N);
-  rep(i, N) {
-    cin >> b[i];
-    mapB[b[i]]++;
+  int N; cin >> N;
+  vector<int> b(N);
+  rep(i, N) cin >> b[i];
+  vector<int> ans;
+  // k番目のボールのうち最も右にあるものを削除する
+  while(b.size() > 0) {
+    int k = 0;
+    for(int i = 1; i <= b.size(); i++) {
+      if (b[i - 1] == i) {
+        chmax(k, i);
+      }
+    }
+    if (k == 0) {
+      cout << -1 << endl;
+      return 0;
+    } else {
+      ans.push_back(k);
+      b.erase(b.begin() + k - 1);
+    }
   }
-
-  // 全部やってみればええんでない？
-  if (!dfs(1)) {
-    cout << - 1 << endl;
-  }
+  reverse(all(ans));
+  print_vector(ans);
 }
-
